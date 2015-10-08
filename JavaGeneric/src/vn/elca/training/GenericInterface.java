68a6a5351132f6c10a0d58a9b90abf3ec8d154b0
ilt <ilentt@ilenlab.com>
2015-10-08 10:28:27 +0700

    update project

---
+++ JavaGeneric/src/vn/elca/training/GenericInterface.java
@@ -0,0 +1,5 @@
+package vn.elca.training;
+
+public interface GenericInterface<G> {
+	public G doSomething();
+}
---
+++ JavaGeneric/src/vn/elca/training/GenericInterfaceImpl.java
@@ -0,0 +1,11 @@
+package vn.elca.training;
+
+public class GenericInterfaceImpl<G> implements GenericInterface<G> {
+
+	@Override
+	public G doSomething() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
